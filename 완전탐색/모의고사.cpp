89cedd9f0c9aa95f977ec37c162cce630cebb17a
rapidswap <73596570+rapidswap@users.noreply.github.com>
2024-10-21 02:20:06 +0900

    Create 모의고사.cpp

---
+++ 완전탐색/모의고사.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    vector<vector<int>> math = {{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5} };
+    vector <int> cnt = { 0,0,0 };
+    for (int i = 0; i < answers.size(); i++)
+    {
+        if (answers[i] == math[0][i % math[0].size()]) cnt[0]++;
+        if (answers[i] == math[1][i % math[1].size()]) cnt[1]++;
+        if (answers[i] == math[2][i % math[2].size()]) cnt[2]++;
+    }
+
+    int max = *max_element(cnt.begin(), cnt.end());
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (cnt[i] == max)
+            answer.push_back(i+1);
+    }
+    return answer;
+}//https://mungto.tistory.com/28
