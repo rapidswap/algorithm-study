66600b6157751689777ae8e670391991410bae7c
rapidswap <73596570+rapidswap@users.noreply.github.com>
2024-10-24 00:39:13 +0900

    Create New_Year_Chaos.cpp

---
+++ HackerRank/New_Year_Chaos.cpp
@@ -0,0 +1,19 @@
+void minimumBribes(vector<int> q) {
+    int cnt=0;
+    bool ischao =false;
+    for(int i = 0; i< q.size(); i++)
+    {
+        if(q[i]>i+3){
+            cout<<"Too chaotic"<<endl;
+            ischao=true;
+            break;
+        }
+        else{
+            for(int j = q[i]-2 > 0? q[i]-2:0; j<i; j++)
+                if(q[j]>q[i]) cnt++;
+        }
+    }
+    if(!ischao)
+        cout<<cnt<<endl;
+    
+}
