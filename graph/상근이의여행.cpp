e6e560dea7e5c615fd78d300be6b24f284e2b3f8
Hyeongjin Kang <73596570+rapidswap@users.noreply.github.com>
2024-11-30 23:18:54 +0900

    Create 상근이의여행.cpp

---
+++ graph/상근이의여행.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+
+    int test_case;
+    cin >> test_case;
+
+    for (int t = 1; t <= test_case; t++)
+    {
+        int N, M;
+        cin >> N >> M;
+
+        for (int i = 1; i <= M; i++)
+        {
+            int a, b;
+            cin >> a >> b;
+
+        }
+        cout << N - 1<<"\n";
+    }
+
+
+   
+    return 0;
+}
