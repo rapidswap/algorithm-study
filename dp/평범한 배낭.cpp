1152413990ad45919aabac2e6320918f83bf96a5
Hyeongjin Kang <73596570+rapidswap@users.noreply.github.com>
2025-07-04 00:04:26 +0900

    Update 평범한 배낭.cpp

--- dp/평범한 배낭.cpp
+++ dp/평범한 배낭.cpp
@@ -3,9 +3,12 @@
 #include <algorithm>
 
 using namespace std;
+#define MAX 110
 int n, m;
-int bag[101][2];
-int dp[100001][101];
+int W[MAX];
+int V[MAX];
+int DP[MAX][100010];
+int dp[101][100001];
 
 int main() {
     ios::sync_with_stdio(0);
@@ -14,13 +17,17 @@ int main() {
     cin >> n >> m;
     
     for (int i = 1; i <= n; i++) {
-        cin >> bag[i][0];
-        cin >> bag[i][1];
+        cin >> W[i] >> V[i];
     }
 
-    for (int i = 1; i <= n; i++) {
-
+    for (int i = 1; i <= n; i++) {                   
+        for (int j = 1; j <= m; j++) {               
+            if (j >= W[i]) {                          
+                DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - W[i]] + V[i]);  
+            }
+            else DP[i][j] = DP[i - 1][j];
+        }
     }
-    
+    cout << DP[n][m] << endl;
     return 0;
 }
